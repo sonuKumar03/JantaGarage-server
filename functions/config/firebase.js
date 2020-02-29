35238d9f9ec54fe5a4ebc9b54639ea0dcfe98744
sonuKumar03 <sonukumar03xt@gmail.com>
2020-02-29 20:38:58 +0530

    demo key -added

--- functions/config/firebase.js
+++ functions/config/firebase.js
@@ -1,14 +1,16 @@
 let firebase = require('firebase');
 
-let firebaseConfig ={
-    apiKey: "AIzaSyCfVv9IZJhm4sGRcFhLL_SB9pYkQladtzA",
-    authDomain: "web-app-3d24a.firebaseapp.com",
-    databaseURL: "https://web-app-3d24a.firebaseio.com",
-    projectId: "web-app-3d24a",
-    storageBucket: "web-app-3d24a.appspot.com",
-    messagingSenderId: "794440820156",
-    appId: "1:794440820156:web:42173cbf02d4fdebdd37b9"
-  };
+
+var firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
 
 firebase.initializeApp(firebaseConfig);
 exports.firebase=firebase;
